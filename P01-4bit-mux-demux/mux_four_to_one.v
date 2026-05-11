6664d3cd19efa7248e26bc47d698a7673fdaa841
Sandip Seal <sandipseal458@gmail.com>
2026-05-11 20:44:36 +0530

    Create mux_four_to_one.v

---
+++ P01-4bit-mux-demux/mux_four_to_one.v
@@ -0,0 +1,20 @@
+module four_to_one_mux(x, y, z, w, sel, out);
+  input [3:0]x;
+  input [3:0]y;
+  input [3:0]z;
+  input [3:0]w;
+  input [1:0]sel;
+  output reg [3:0]out;
+  
+  always @(*)
+    begin
+      case (sel)
+         2'b00:  out = x;
+         2'b01:  out = y;
+         2'b10:  out = z;
+         2'b11:  out = w;
+      endcase
+    end   
+endmodule
+           
+        
